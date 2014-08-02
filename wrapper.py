dd4bb4956f5225ba50c9569e584b34744aa92602
Sashko Stubailo <sashko@mit.edu>
2014-08-02 15:35:05 -0700

    Always creates application support folder on startup

--- wrapper.py
+++ wrapper.py
@@ -1,3 +1,10 @@
-import subprocess, sys
+import subprocess
+import os
+import sys
+
+common_path = os.path.expanduser("~/Library/Application Support/git-undo/")
+
+if not os.path.isdir(common_path):
+  os.mkdir(common_path)
 
 subprocess.call(["git"] + sys.argv[1:])
