4efa98520b4d3a042da3da54d1d4e48aa78c7e4d
Sashko Stubailo <sashko@mit.edu>
2014-08-02 15:40:48 -0700

    Added some comments

--- wrapper.py
+++ wrapper.py
@@ -2,8 +2,10 @@ import subprocess
 import os
 import sys
 
+# folder to store all settings and backups
 common_path = os.path.expanduser("~/Library/Application Support/git-undo/")
 
+# make sure the settings and backups folder exists
 if not os.path.isdir(common_path):
   os.mkdir(common_path)
 
